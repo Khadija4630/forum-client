260271c3b8c04cf98af679458d66d516d32e08a1
Khadija4630 <khadijahaque085@gmail.com>
2025-01-28 01:04:12 -0500

    settig admin route

--- src/Components/Routes/AdminRoute.jsx
+++ src/Components/Routes/AdminRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, useLocation } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
-// import useAdmin from "../hooks/useAdmin";
+import useAdmin from "../Hooks/useAdmin";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
 
 
 const AdminRoute = ({ children }) => {
-    const { user, loading } = useAuth();
+    const { user, loading } = useContext(AuthContext);
     const [isAdmin, isAdminLoading] = useAdmin();
     const location = useLocation();
 
